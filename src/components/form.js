186f2391d0f14d50324ce73be33b441ea1291a5d
Michael <meyermichael507@gmail.com>
2021-10-11 17:24:43 -0500

    added storybook and webpack 4

--- src/components/form.js
+++ src/components/form.js
@@ -6,33 +6,41 @@ function ContactForm() {
       return <p>Thanks for joining!</p>;
   }
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
+    <form class="hidden md:flex flex-wrap flex-row justify-around bg-white rounded-lg px-4 py-8 w-full max-w-2xl" onSubmit={handleSubmit}>
+    <div class="flex items-center border-b py-3 w-1/4">
+      <input class="appearance-none bg-transparent border-none w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="NOMBRE" id="name"
+         
+        name="name"/>
+                      
+    </div>
+
+    <div class="flex items-center border-b  py-3 w-1/3">
+      <input class="appearance-none bg-transparent border-none w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="CORREO" id="email"
+        
+        name="email"/>
+                
+      
+    </div>
+
+    <div class="flex items-center border-b py-3 w-1/4">
+      <input class="appearance-none bg-transparent border-none w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="TELÉFONO" id="phone"
+        
+        name="phone"/>
+                
+      
+    </div>
+    
+    <div class="flex items-center border-b py-2 w-4/6">
+      <input class="appearance-none bg-transparent border-none w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="PRODUCTO" id="producto"
+         
+        name="producto"/>
+      
+      
+    </div>
+    <button class="flex-shrink-0 gradient hover:bg-teal-700 border-teal-600  hover:border-teal-700 text-sm border-4 text-white  px-3 mt-4     rounded" type="button">
+        COTIZAR
       </button>
-    </form>
+  </form>
   );
 }
 
