482c816e8ec31cfd548060d2a412151a227b9e89
Michael <meyermichael507@gmail.com>
2022-08-01 15:39:56 -0500

    signo de exclamacion

--- src/components/layout.js
+++ src/components/layout.js
@@ -215,7 +215,7 @@ function Layout({ children }) {
               
               <div className="container mx-auto flex flex-wrap pt-4 pb-12">
               
-                <h3 className="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800 mb-12" data-aos="fade-up">¡Aprovecha nuestras <span className="gradient2 text-white font-normal py-1 px-1">PROMOCIONES</span> del Mes</h3>
+                <h3 className="w-full my-2 text-3xl font-bold leading-tight text-center text-gray-800 mb-12" data-aos="fade-up">¡Aprovecha nuestras <span className="gradient2 text-white font-normal py-1 px-1">PROMOCIONES</span> del Mes!</h3>
                 {/* Separador <div className="w-full mb-4">	
                   <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                 </div> */}
